427850e9ffeda863d430da24a4967ae0a8be38d8
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-15 12:31:05 +0100

    adding restricted resource endpont

--- P01_initial_attempt/springapp/src/main/java/com/example/DemoController.java
+++ P01_initial_attempt/springapp/src/main/java/com/example/DemoController.java
@@ -1,7 +1,10 @@
 package com.example;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authorization.method.AuthorizeReturnObject;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo")
 public class DemoController {
 
+    @GetMapping("/resource/{resourceId}")
+    @PreAuthorize("hasRole('ROLE_my-app-admin') || hasRole('ROLE_my-app-user') && #resourceId == authentication.name")
+    public Response getResource(@PathVariable String resourceId) {
+        return new Response("Resource:" + resourceId + " USER:" + SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
     @GetMapping("/user")
-    @PreAuthorize("hasRole('my-app-user')")
+    @PreAuthorize("hasRole('ROLE_my-app-user')")
     public Response user() {
-        return new Response("HELLO USER");
+        return new Response("HELLO USER:" + SecurityContextHolder.getContext().getAuthentication().getName());
     }
 
     @GetMapping("/admin")
-    @PreAuthorize("hasRole('my-app-admin')")
+    @PreAuthorize("hasRole('ROLE_my-app-admin')")
     public Response admin() {
-        return new Response("HELLO ADMIN");
+        return new Response("HELLO ADMIN:" + SecurityContextHolder.getContext().getAuthentication().getName());
     }
 }
 
